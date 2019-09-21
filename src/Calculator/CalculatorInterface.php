6edbb6e6548d0fa27bbac82a09be5166e067b458
Natali <natkraskras@bk.ru>
2019-09-21 17:31:36 +0300

    добавлены комментарии к методу вычитания

--- src/Calculator/CalculatorInterface.php
+++ src/Calculator/CalculatorInterface.php
@@ -9,6 +9,12 @@ interface CalculatorInterface
      * @var float $a первое слагаемое
      */
     public function addition(float $a, float $b): float;
+    
+    /**
+     * высчитывает разность 2х чисел
+     * @var float $a делимое
+     * @return 
+     */
     public function minus(float $a, float $b): float;
     public function multiplication(float $a, float $b): float;
     public function divide(float $a, float $b): float;
